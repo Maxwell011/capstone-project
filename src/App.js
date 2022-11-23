79e2891ce6844af1601c10f41976096333543f8d
Orji Maxwell <chukwuemekaorji678@gmail.com>
2022-11-24 00:52:05 +0100

    Added style to the home page

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
-import './App.css';
+import "./App.css";
+import React from "react";
+import Home from "./components/Home";
+import Post from "./components/Post";
+import { Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello REact</h1>
+    <div className='App'>
+      <Routes>
+        <Route path='/' exact element={<Home />} />
+        <Route path='/post/:id' element={<Post />} />
+      </Routes>
     </div>
   );
 }
